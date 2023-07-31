533ce2a138a61bae9a3fccc5392a2901d8260772
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-07-31 19:28:17 +0200

    ADD: __init__.py: added specifications.

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+GAPool is a genetic algorithm using the `numpy`-package. In contrast to standard genetic algorithm, this version is able
+to find a collection of optimal "people", which show a "fitness" within a prescribed "deficit" of the "fittest person".
+
+Author: Gijs G. Hendrickx
+"""
+__all__ = ['src.ga.GeneticAlgorihm']
+
+__version__ = '1.0'
+__author__ = 'Gijs G. Hendrickx'
+__description__ = 'Genetic algorithm with "best-pool" functionality'
